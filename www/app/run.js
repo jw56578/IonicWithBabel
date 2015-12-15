35b55ce61aaadfb1fb4d3b0faff512a6fc280542
jw56578 <jw56578@gmail.com>
2015-12-14 19:19:59 -0600

    sdfdsf

--- www/app/run.js
+++ www/app/run.js
@@ -1,8 +1,6 @@
  
 export default function config($ionicPlatform){
     $ionicPlatform.ready(function() {
-        alert('time to redirect');
-        location.href="http://www.google.com";
         return;
         if(window.cordova && window.cordova.plugins.Keyboard) {
           // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
